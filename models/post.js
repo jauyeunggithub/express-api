813ba21499de7034d7b5cf92b5aedfe27355281e
Ho Hang Au Yeung <mayeung@telus.net>
2025-07-21 15:32:08 -0700

    rename files

---
+++ models/post.js
@@ -0,0 +1,18 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+const User = require("./user");
+
+const Post = sequelize.define("Post", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+Post.belongsTo(User); // User can have many Posts
+
+module.exports = Post;
